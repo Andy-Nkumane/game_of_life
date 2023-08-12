5f4da663bf895f1e58f0e5bc0b7269383a9bbc6f
Andy Nkumane <nkumaneandy@gmail.com>
2023-08-12 13:48:35 +0200

    formatted code using black

--- game_of_life.py
+++ game_of_life.py
@@ -3,8 +3,7 @@ import time
 
 BLOCK_DIMENSION = 20
 
-gol = turtle.Turtle()
-gol.hideturtle()
+turtle.hideturtle()
 screen = turtle.Screen()
 turtle.pencolor("gray")
 
@@ -17,18 +16,18 @@ game_grid = [
     [0, 0, 1, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 1, 1, 0],
     [0, 0, 0, 0, 0, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0]
+    [0, 0, 0, 0, 0, 0, 1, 0, 0],
 ]
 
 neighbours = [
-    (0, 1), # right
-    (0, -1), # left
-    (-1, 0), # top
-    (1, 0), # bottom
-    (-1, -1), # top left
-    (1, -1), # bottom left
-    (1, 1), # bottom right
-    (-1, 1)  # top right
+    (0, 1),  # right
+    (0, -1),  # left
+    (-1, 0),  # top
+    (1, 0),  # bottom
+    (-1, -1),  # top left
+    (1, -1),  # bottom left
+    (1, 1),  # bottom right
+    (-1, 1),  # top right
 ]
 
 
@@ -51,11 +50,14 @@ def get_obstacles():
     """returns the list of obstacles."""
     obstacles = []
     constant = len(game_grid) // 2
-    indexes = range(-constant, constant+1)
+    indexes = range(-constant, constant + 1)
     for i_row, row in enumerate(game_grid):
         for i, v in enumerate(row):
             if not row[i]:
-                coordinates = (get_coordinate(i, indexes), get_coordinate(i_row, indexes[::-1]))
+                coordinates = (
+                    get_coordinate(i, indexes),
+                    get_coordinate(i_row, indexes[::-1]),
+                )
                 if not None in coordinates:
                     obstacles.append(coordinates)
     return obstacles
@@ -65,11 +67,14 @@ def get_grid():
     grid = []
     grid_matrix = []
     constant = len(game_grid) // 2
-    indexes = range(-constant, constant+1)
+    indexes = range(-constant, constant + 1)
     for i_row, row in enumerate(game_grid):
         row_list = []
         for i, v in enumerate(row):
-            coordinates = (get_coordinate(i, indexes), get_coordinate(i_row, indexes[::-1]))
+            coordinates = (
+                get_coordinate(i, indexes),
+                get_coordinate(i_row, indexes[::-1]),
+            )
             row_list.append(coordinates)
         grid.extend(row_list)
         grid_matrix.append(row_list)
@@ -120,10 +125,10 @@ def game_of_life():
             neighbour_count = check_neighbours(row, col)
             if game_grid[row][col] == 1:
                 if is_death(neighbour_count):
-                    death_list.append((row,col))
+                    death_list.append((row, col))
             else:
                 if is_birth(neighbour_count):
-                    birth_list.append((row,col))
+                    birth_list.append((row, col))
 
     update_grid(death_list, birth_list)
 
@@ -132,7 +137,8 @@ def update_grid(death_list, birth_list):
     for row, col in death_list:
         game_grid[row][col] = 0
     for row, col in birth_list:
-        game_grid[row][col] = 1  
+        game_grid[row][col] = 1
+
 
 grid_coord, grid_matrix = get_grid()
 
